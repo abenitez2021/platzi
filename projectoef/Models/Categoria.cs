e46be8738e55125cd7787815fc9f063ab87b2004
abenitez2021 <aldonoel2012@gmail.com>
2023-05-02 14:03:17 -0400

    Segunda version del proyectoef

--- projectoef/Models/Categoria.cs
+++ projectoef/Models/Categoria.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace proyectoef.Models;
 public class Categoria {
+    [Key]
     public Guid CategoriaId{set;get;}
+    
+    [Required]
+    [MaxLength(150)]
     public string Nombre {get;set;}
     public string Descripcion {get;set;}
     public virtual ICollection<Tarea> Tareas{get;set;}
