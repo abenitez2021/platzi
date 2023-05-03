0f52facfa35950bd78d547df480f65c4630dcb66
abenitez2021 <aldonoel2012@gmail.com>
2023-05-03 12:26:33 -0400

    tercera version del proyectoef

--- projectoef/Program.cs
+++ projectoef/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoef;
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<TareasContext>(p=> p.UseInMemoryDatabase("TareasDB"));
+builder.Services.AddSqlServer<TareasContext>("Data Source=DESKTOP-KBG54GH\\MSSQLSERVER2019;Initial Catalog= TareasDb;Trusted_Connection=True; Integrated Security=True;TrustServerCertificate=True");
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
